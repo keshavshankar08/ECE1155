ee1bd58b228467531c6ba63629dac7374fa2a865
Keshav Shankar <keshavshankar08@gmail.com>
2025-04-01 12:00:26 -0400

    layout for class

---
+++ CredentialStuffing/CredentialStuffing.py
@@ -0,0 +1,33 @@
+
+
+class CredentialStuffing:
+    def __init__(self):
+        pass
+
+    def load_database(self):
+        pass
+
+    def start_attack(self, username, password):
+        pass
+
+    def attack(self):
+        # TODO Clean up credentials
+        # TODO Search against database
+        # if (hit found): return stats crack success
+        # else:
+            # TODO Generate varied credentials
+            # TODO Search against databsae
+            # if (hit found): return stats crack success
+            # else: return crack failed
+        pass
+
+    def clean_credentials(self):
+        pass
+
+    def search_database(self):
+        pass
+
+    def generate_varied_credentials(self):
+        pass
+
+
